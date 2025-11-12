b3f3081f6f1467a571251dcf10e7d685b3ce2163
JoealAJ <ajjoeal@gmail.com>
2025-11-12 16:38:29 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./components/Card.css";
@@ -8,15 +7,13 @@ import CartPage from "./components/CartPage";
 
 function App() {
   return (
-    <>
-    
+    <Router>
       <Routes>
         <Route path="/" element={<Card />} />
         <Route path="/details/:id" element={<Details />} />
         <Route path="/cart" element={<CartPage />} />
       </Routes>
-    
-    </>
+    </Router>
   );
 }
 
@@ -40,4 +37,3 @@ export default App;
 // }
 
 // export default App;
-
